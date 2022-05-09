b86c09a493e4f7ba3b2a57a4f1b997170e6fc72c
Goodla <45775313+Goodla@users.noreply.github.com>
2022-05-09 12:26:01 -0700

    Update 1523_CountOddNumbersInAnIntervalRange.py

--- python/1523_CountOddNumbersInAnIntervalRange.py
+++ python/1523_CountOddNumbersInAnIntervalRange.py
@@ -5,9 +5,23 @@ class Solution(object):
         :type high: int
         :rtype: int
         """
+        #15ms, 13.4MB
+        """
         c = 0
         
         if low%2 == 1 or high%2 == 1:
             c += 1
         c += ((high-low)/2)
         return c
+        """
+        
+        #43ms, 13.4MB
+        #return ((high-low)/2) + (0, 1)[low%2 == 1 or high%2 == 1]
+        
+        
+        
+        #21 ms, 13.5MB
+        #return (0 + (high-low)/2, 1 + (high-low)/2)[low%2 == 1 or high%2 == 1]
+    
+        #15ms, 13.3MB
+        return (high+1)/2 - low/2
